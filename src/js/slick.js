9ca8349a11f60648ca06bd2fb51d21ed3773f7f1
Nazar Talaievych <nazartalaievych@gmail.com>
2023-12-21 21:59:58 +0200

    carousel

---
+++ src/js/slick.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevrons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevrons/right.svg"></button>'
+    })
+});
