50c61034829207c8e74aeccdbfdcdd6c5a2c3bed
Nazar Talaievych <nazartalaievych@gmail.com>
2023-12-24 21:22:35 +0200

    modal windows

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1200,
         prevArrow: '<button type="button" class="prev"><img src="icons/chevrons/left.svg"></button>',
