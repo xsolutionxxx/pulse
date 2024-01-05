7597c0b2d67537d87b4b18437eef7844547215d6
Nazar Talaievych <nazartalaievych@gmail.com>
2024-01-06 01:03:27 +0200

    mailer, animation, pageup and other

---
+++ src/js/mailer.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    })
+});
--- src/js/swiper.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const swiper = new Swiper(".pulseSwiper", {
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-});
